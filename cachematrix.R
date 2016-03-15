b6ea4231f74880089e216f76d84f097d9bf9e430
RokIvansek <ivansek.rok@gmail.com>
2016-03-16 00:00:05 +0100

    added body of functions makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions make use of the scoping rules of the R language
+## to preserve states inside an R object
 
-## Write a short comment describing this function
+## This function constructs a special object (but actualy a list :),
+## that contains functions (methods) for:
+##    -setting the value of the matrix,
+##    -getting the value of the matrix,
+##    -setting the value of the invers,
+##    -getting the value of the invers.
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL #if we change the data the old invers won't do anymore
+  }
+  get <- function() x
+  setinvers <- function(solve) i <<- solve
+  getinvers <- function() i
+  list(set = set, get = get,
+       setinvers = setinvers,
+       getinvers = getinvers)
 
 }
 
-
-## Write a short comment describing this function
+## This function checks to see if the invers of the matrix has allready
+## been calculated and if it hasn't been it calculates it and updates the
+## special object given in input.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinvers()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinvers(i)
+  i
 }
